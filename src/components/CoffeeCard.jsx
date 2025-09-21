7aa48b490df844ab72780d9959ab60e612b5c9c4
Rajvishah12 <rajvis2011@gmail.com>
2025-09-21 04:21:48 -0500

    feat: added images

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,9 +1,12 @@
 const CoffeeCard = (props) => {
   return (
   <div className="oneShop">
+    <img src={props.image}/>
+    <div className = "writtenInfo">
     <h2>{props.name}</h2>
     <p>{props.address}</p>
     <p>{props.number}</p>
+    </div>
   </div>
 )
 }
